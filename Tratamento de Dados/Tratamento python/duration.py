e678c0336e87ac232b1d2e2a4adf982d681dcf3b
wesleyruanwr <119066770+wesleyruanwr@users.noreply.github.com>
2024-05-02 13:12:52 -0300

    tratamento de dados

---
+++ Tratamento de Dados/Tratamento python/duration.py
@@ -0,0 +1,13 @@
+'''
+a coluna duration tinha numeros e letras dentro dela e era dividida em minutos e seasons.
+tiramos todos os caracteres que nao fossem numericos de dentro dela e transformamos ela para INT
+
+df['duration'] = df['duration'].replace(r'\D', '', regex=True)
+
+df['duration'] = df['duration'].replace('', pd.NA)
+
+df['duration'] = df['duration'].fillna(0)
+
+df['duration'] = df['duration'].astype(int)
+
+'''
